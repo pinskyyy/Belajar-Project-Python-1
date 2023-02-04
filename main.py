c8635d4cbd7ea5100af5d718083f0dca825519b3
pinskyyy <kevindwikos@gmail.com>
2023-02-04 18:45:18 +0700

    pembaruan

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
 from modular import Transaction
 
-user = input("Masukan nama anda: ")
+print("Selamat datang di Cashier Pacmann".center(40,'='))
+user = input("Masukan nama Anda: ")
 user = Transaction()
 
-user.menu()
+while True:
+    print('-'*40)
+    print(" Super Cashier ".center(40,'='))
+    print('-'*40)
+    print("1. Add Item")
+    print("2. Update Item Name")
+    print("3. Update Item Quantity")
+    print("4. Update Item Price")
+    print("5. Delete Item")
+    print("6. Reset Trx")
+    print("7. Check Order")
+    print("8. Calculate Total Price")
+    print("9. Exit")
+    print('-'*40)
+
+    pilihan = input("Masukan Pilihan Anda = ")
+    if pilihan == "1":
+        nama = input("Masukan nama barang: ")
+        qty = int(input("Masukan qty barang: "))
+        harga = int(input("Masukan harga barang: "))
+        user.add_items(nama, qty, harga)
+    elif pilihan == "2":
+        nama_lama = input("Masukan nama barang yang ingin anda update: ")
+        nama_baru = input("Masukan nama barang terbaru: ")
+        user.update_item_name(nama_lama, nama_baru)
+    elif pilihan == "3":
+        nama = input("Masukan nama barang yang ingin anda update qty nya: ")
+        qty_baru = int(input("Masukan qty terbaru: "))
+        user.update_qty_item(nama, qty_baru)
+    elif pilihan == "4":
+        nama = input("Masukan nama barang yang ingin anda update qty nya: ")
+        harga_baru = int(input("Masukan harga terbaru: "))
+        user.update_price_item(nama, harga_baru)
+    elif pilihan == "5":
+        nama = input("Masukan nama barang yang ingin anda hapus: ")
+        user.delete_item(nama)
+    elif pilihan == "6":
+        user.reset_trx()
+    elif pilihan == "7":
+        user.check_order()
+    elif pilihan == "8":
+        user.total_price()
+    elif pilihan == "9":
+        print("Terimakasih telah berbelanja di Pacmann Cashier")
+        break
+    else:
+        print("Masukan hanya dengan angka menu yang tersedia!")
--- modular.py
+++ modular.py
@@ -8,46 +8,28 @@ class Transaction():
     keranjang = {}
     total_bayar = 0
                 
-    def add_items(self):
-        while True:
-            a = input("Masukan nama barang: ")
-            b = int(input("Masukan qty barang: "))
-            c = int(input("Masukan harga barang: "))
-            print(f'Item {a} berhasil ditambahkan ke dalam keranjang!')
-            self.keranjang[a] = [b,c]
-            sub_total = b * c
+    def add_items(self, nama, qty, harga):
+            print(f'Item {nama} berhasil ditambahkan ke dalam keranjang!')
+            self.keranjang[nama] = [qty,harga]
+            sub_total = qty * harga
             self.total_bayar += sub_total
-            lagi = input("Ingin menambah item lagi? (y/n): ")
-            if lagi != "y":
-                user.menu()
         
-    def update_item_name(self):
-        nama_lama = input("Masukan nama barang yang ingin anda update: ")
-        nama_baru = input("Masukan nama barang terbaru: ")
+    def update_item_name(self, nama_lama, nama_baru):
         self.keranjang[nama_baru] = self.keranjang[nama_lama]
         print(f'{nama_lama} berhasil diupdate menjadi {nama_baru}')
         del self.keranjang[nama_lama]
-        user.menu()
         
-    def update_qty_item(self):
-        a = input("Masukan nama barang yang ingin anda update qty nya: ")
-        qty_baru = int(input("Masukan qty terbaru: "))
-        self.keranjang[a][0] = qty_baru
-        print(f'Qty {a} berhasil diupdate menjadi {qty_baru}')
-        user.menu()
+    def update_qty_item(self, nama, qty_baru):
+        self.keranjang[nama][0] = qty_baru
+        print(f'Qty {nama} berhasil diupdate menjadi {qty_baru}')
         
-    def update_price_item(self):
-        a = input("Masukan nama barang yang ingin anda update qty nya: ")
-        price_baru = int(input("Masukan harga terbaru: "))
-        self.keranjang[a][1] = price_baru
-        print(f'Harga {a} berhasil diupdate menjadi {price_baru}')
-        user.menu()
+    def update_price_item(self, nama, harga_baru):
+        self.keranjang[nama][1] = harga_baru
+        print(f'Harga {nama} berhasil diupdate menjadi {harga_baru}')
         
-    def delete_item(self):
-        a = input("Masukan nama barang yang ingin anda hapus: ")
-        print(f'{a} berhasil dihapus!')
-        del self.keranjang[a]
-        user.menu()
+    def delete_item(self, nama):
+        print(f'{nama} berhasil dihapus!')
+        del self.keranjang[nama]
         
     def reset_trx(self):
         yakin = input("Apakah anda yakin ingin menghapus semua item?(y/n): ")
@@ -55,14 +37,12 @@ class Transaction():
             self.keranjang.clear()
             print("Semua item berhasil direset")
         if yakin != "y":
-            user.menu()
+            pass
     def check_order(self):
         df = pd.DataFrame(self.keranjang,["Qty","Harga/item"])
         data = df.T
         data["Total"] = data["Qty"] * data["Harga/item"]
         display(data)
-
-        user.menu()
         
     def total_price(self):
         discount = 0
@@ -74,53 +54,3 @@ class Transaction():
             discount = self.total_bayar * 0.10
         final_price = self.total_bayar - discount
         print(f'Item yang dibeli {self.keranjang}\nTotal yang harus dibayarkan adalah Rp {final_price}')
-        
-        user.menu()
-
-    def exit(self):
-        print("Terimakasih telah berbelanja!")
-        return
-
-    def test(self):
-        subtotal = self.keranjang.keys[0]
-        print(subtotal)
-
-    def menu(self):
-        print('-'*40)
-        print(" Super Cashier".center(40,'='))
-        print('-'*40)
-        print("1. Add Item")
-        print("2. Update Item Name")
-        print("3. Update Item Quantity")
-        print("4. Update Item Price")
-        print("5. Delete Item")
-        print("6. Reset Trx")
-        print("7. Check Order")
-        print("8. Calculate Total Price")
-        print("9. Exit")
-        print('-'*40)
-        pilihan = input("Masukan Pilihan Anda = ")
-        if pilihan == "1":
-            user.add_items()
-        elif pilihan == "2":
-            user.update_item_name()
-        elif pilihan == "3":
-            user.update_qty_item()
-        elif pilihan == "4":
-            user.update_price_item()
-        elif pilihan == "5":
-            user.delete_item()
-        elif pilihan == "6":
-            user.reset_trx()
-        elif pilihan == "7":
-            user.check_order()
-        elif pilihan == "8":
-            user.total_price()
-        elif pilihan == "9":
-            user.exit()
-        elif pilihan == "10":
-            user.test()
-        else:
-            print("Masukan hanya dengan angka menu yang tersedia!")
-
-user = Transaction()
